932ce98b2384b6807367337d4cbddd5e16c9d1ae
endryo <jandersonendryo@hotmail.com>
2022-12-10 19:57:47 -0300

    detalhes

--- soft.test.js
+++ soft.test.js
@@ -5,6 +5,8 @@ const request = require('supertest')
 describe('teste', () => {
     it('teste', async () => {
         const res = await request(app).get('/')
+
         expect(res.statusCode).toEqual(200)
+        console.log(res)
     })
 })
